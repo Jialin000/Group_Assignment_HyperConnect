8a5c542d5398259eaca75066f8975904d1fcd0eb
Jialin000 <61930223+Jialin000@users.noreply.github.com>
2020-04-16 19:37:48 +1000

    test_demo

---
+++ index.js
@@ -0,0 +1,53 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json()); 
+
+app.get('/', (req,res) =>{
+    res.send('Hellow World!!!!!!')
+});
+
+
+// Two argument
+//'/'-the path, 
+//A callback function(req,res).., or route handler
+
+const courses = [
+    {id :1, name: 'course1'},
+    {id :2, name: 'course2'},
+    {id :3, name: 'course3'}
+];
+
+app.get('/api/courses', (req, res)=>{
+    res.send(courses);
+});
+
+app.post('/api/courses',(req, res) =>{
+    const schema ={
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };    
+    courses.push(course);
+    res.send(course); 
+}) //we want to put in collection of the courses
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('The given ID have not found')// 404
+    res.send(course)
+});
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(port, console.log(`Listening on port ${port}...`))
