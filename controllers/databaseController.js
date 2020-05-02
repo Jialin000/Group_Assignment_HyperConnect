54a6916860667a9195eb314681a90d8480886eea
Jialin000 <61930223+Jialin000@users.noreply.github.com>
2020-05-02 20:58:52 +1000

    Revert "Update databaseController.js"
    This reverts commit 11183383a45b37de43985c1faa733ae32151cdea, reversing
    changes made to 0e21c67044da87c3c2db802689937c5c2c9db7c4.

---
+++ controllers/databaseController.js
@@ -0,0 +1,42 @@
+//require to the mongoose and load the pre-defined model
+const mongoose = require("mongoose");
+const fetch = require('node-fetch');
+
+// functions to handle different requests for bays related resources
+const receiveRequest = (req, res, next) => {
+    res.statusCode = 200;
+    next();
+};
+
+
+const updateDatabase = (req, res, next) => {
+    
+    const db = mongoose.connection;
+    let url = "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json?%24limit=5000&%24%24app_token=AdyvI0gKUddcE1r6gTOaarTiX";
+    let settings = { method: "Get" };
+    let bays = [];
+    fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            for(let i_data = 0; i_data < json.length; i_data++){
+                const bay = {
+                  bay_id: json[i_data]["bay_id"],
+                  st_marker_id: json[i_data]["st_marker_id"],
+                  status: json[i_data]["status"],
+                  location: json[i_data]["location"],
+                  lat: json[i_data]["lat"],
+                  lon: json[i_data]["lon"]
+                }
+                bays.push(bay)
+                db.collection('parkingBays').updateOne({bay_id:bay["bay_id"]},{$set:bay},{upsert: true});
+              };
+        return res.send(bays.length+" records from database have been updated");
+    })
+};
+
+
+
+module.exports = {
+    receiveRequest,
+    updateDatabase
+}
