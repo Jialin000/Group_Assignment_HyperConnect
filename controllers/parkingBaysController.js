24c3e9576a14c38da7c7cef708e51ad8cd75720f
Jialin000 <61930223+Jialin000@users.noreply.github.com>
2020-05-09 12:24:56 +1000

    add database access

--- controllers/parkingBaysController.js
+++ controllers/parkingBaysController.js
@@ -10,6 +10,21 @@ const getBays = async(req, res, next) => {
         // get information about parking bay from database
         const all_bays = await Bays.find();
         res.statusCode = 200;
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+
         return res.send(all_bays);
       } catch (err) {
         res.status(400);
