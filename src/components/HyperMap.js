d37eca27577a799980cd20d4486bf63fb087064b
Jialin000 <61930223+Jialin000@users.noreply.github.com>
2020-05-10 20:34:02 +1000

    search address function with a small problem

--- src/components/HyperMap.js
+++ src/components/HyperMap.js
@@ -2,9 +2,13 @@ import React, { Component,useState, useEffect } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import {Marker,InfoWindow} from '@react-google-maps/api';
 import {usePosition} from 'use-position';
-
+import Autocomplete from 'react-google-autocomplete';
 // Display parking bays on map
+
+
 export default function HyperMap(props) {
+
+    
 
     // style of map
     const containerStyle = {
@@ -24,14 +28,41 @@ export default function HyperMap(props) {
     } = usePosition(watch, {enableHighAccuracy: true});
 
     // center of map
-    const center = {
+    let center = {
         lat: latitude,
         lng: longitude
     }
 
     const [selectedPark, setSelectedPark] = useState(null);
-
+    
     // render
+
+    // onPlaceSelected = ( place ) => {
+    //     console.log( 'plc', place );
+    //     const address = place.formatted_address,
+    //           addressArray =  place.address_components,
+    //           city = this.getCity( addressArray ),
+    //           area = this.getArea( addressArray ),
+    //           state = this.getState( addressArray ),
+    //           latValue = place.geometry.location.lat(),
+    //           lngValue = place.geometry.location.lng();
+    //     // Set these values in the state.
+    //     this.setState({
+    //         address: ( address ) ? address : '',
+    //         area: ( area ) ? area : '',
+    //         city: ( city ) ? city : '',
+    //         state: ( state ) ? state : '',
+    //         markerPosition: {
+    //             lat: latValue,
+    //             lng: lngValue
+    //         },
+    //         mapPosition: {
+    //             lat: latValue,
+    //             lng: lngValue
+    //         },
+    //     })
+    // }
+
     return (
         <LoadScript
             googleMapsApiKey="AIzaSyCE_by6BiXR1XCws5YiduStyJfvzPrXfuc"
@@ -41,6 +72,18 @@ export default function HyperMap(props) {
                 center={center}
                 zoom={16}
             >
+                {/* <Autocomplete
+					style={{
+					    width: '100%',
+						height: '40px',
+						paddingLeft: '16px',
+						marginTop: '2px',
+						marginBottom: '100px'
+						}}
+                        onPlaceSelected={ this.onPlaceSelected }
+						types={['(regions)']}
+				/> */}
+                
                 {/* User's current location */}
                 <Marker position={center} label={"Your current location"} 
                 icon={{url:'/userLocationIcon.png'}}/>
@@ -50,7 +93,7 @@ export default function HyperMap(props) {
                 (bay.status === "Unoccupied" &&
                     <Marker
                         position={{lat: parseFloat(bay.lat), lng: parseFloat(bay.lon)}}
-                        onClick={() => {setSelectedPark(bay);}}
+                        onClick={() => {setSelectedPark(bay)}}
                         icon={{url:'/availableIcon.png'}}
                     />)
                 )}
@@ -63,11 +106,12 @@ export default function HyperMap(props) {
                     />)
                 )}
 
-                {selectedPark &&(
+                {selectedPark && 
                     <InfoWindow position={{lat: parseFloat(selectedPark.lat), lng: parseFloat(selectedPark.lon)}}
                     onCloseClick={() => {
                         setSelectedPark(null);
-                    }}>
+                    }}
+                    >
                         <div>
                             <h2>Bay #{selectedPark.bay_id}</h2>
                             <p>st_marker_id: {selectedPark.st_marker_id}</p>
@@ -75,10 +119,16 @@ export default function HyperMap(props) {
                             <p>latitude: {selectedPark.lon}</p>
                         </div>
                     </InfoWindow>
-                )}
+                }
+                
+                {/* {selectedPark && 
+                (this.center.lat=parseFloat(selectedPark.lat))&& 
+                (this.center.lng = parseFloat(selectedPark.lon))
+                } */}
 
             </GoogleMap>
         </LoadScript>
     )
+
 
 }
