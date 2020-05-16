78b2eff7e60afce3331c6c310184c2ae0defdc75
TroyFu <61963705+TroyF22@users.noreply.github.com>
2020-05-17 05:13:04 +1000

    1st version css
    adjustment of pages

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -1,20 +1,18 @@
 import React from "react";
 import SignInForm from "../components/SignInForm"
-
 import { isAuthenticated } from "../userAPI";
-
 import Button from "../components/Button";
 import "../styles.css"
 
 export default function SignInPage() {
-  if (isAuthenticated('Authorization')){
-    return (
-      <div>
-        you have already logged in!
-      </div>
-    );
-  } else{ 
-    return (
+    if (isAuthenticated('Authorization')){
+        return (
+            <div>
+                you have already logged in!
+            </div>
+        );
+    } else{
+        return (
       <div className="login_bg">
           <div className="login_box">
              <SignInForm />
