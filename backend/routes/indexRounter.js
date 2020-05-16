7639af076e0e82010c30928fe0cc236953340aa0
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-17 01:38:01 +1000

    test

--- backend/routes/indexRounter.js
+++ backend/routes/indexRounter.js
@@ -6,22 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('../frontend/public/index');
 });
 
-
 router.get('/parkingBays', function(req, res, next) {
   res.render('../frontend/public/index');
 });
-
-router.get('/users/login', function(req, res, next) {
-  res.render('../frontend/public/index');
-});
-
-router.get('/users/logout', function(req, res, next) {
-  res.render('../frontend/public/index');
-});
-
-router.get('/users/signin', function(req, res, next) {
-  res.render('../frontend/public/index');
-});
-
 
 module.exports = router;
