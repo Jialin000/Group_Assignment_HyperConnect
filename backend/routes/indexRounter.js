b09a7fdbd5ab72994aff8dc44cfe7144af89278e
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-17 02:24:02 +1000

    test

--- backend/routes/indexRounter.js
+++ backend/routes/indexRounter.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* render the page */
-router.get('/*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('../frontend/public/index');
 });
+
+router.get('/users/login', function(req, res, next) {
+  res.render('../frontend/public/index');
+});
+
 
 module.exports = router;
