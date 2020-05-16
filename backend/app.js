f4444203c64feb0af3f3a5192575fd1a817754ec
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-17 01:07:36 +1000

    test

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,6 @@ const app = express();
 require("./models");
 
 // connect to the front end
-
-/*app.set('views', path.join(__dirname, 'frontend'));
-app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);*/
-//app.use(express.static(path.join(__dirname, '../frontend/public/index.html')));
-
 app.set('views', path.join(__dirname, '../frontend/public'));
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
