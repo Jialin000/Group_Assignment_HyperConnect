44d9d08a706c0ecd7420425ba19ad837afd6696b
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-05-17 01:09:39 +1000

    Build nd modifies homepage for frontend

---
+++ frontend/build/precache-manifest.8db20d457338de5c998cd3c893765731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a55113c560388614224d2baa0bd778d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34194e60db8205f4637b",
+    "url": "/static/css/main.27d15f6e.chunk.css"
+  },
+  {
+    "revision": "4ba8d507cccb2bdac104",
+    "url": "/static/js/2.4739a112.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/static/js/2.4739a112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34194e60db8205f4637b",
+    "url": "/static/js/main.307dea33.chunk.js"
+  },
+  {
+    "revision": "3c798523726527972739",
+    "url": "/static/js/runtime-main.77e84ea2.js"
+  }
+]);
