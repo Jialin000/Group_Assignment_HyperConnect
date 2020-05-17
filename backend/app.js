b2d0d6dd9760eb9f29409f0330bafaa105059c4f
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-17 14:08:02 +1000

    test

--- backend/app.js
+++ backend/app.js
@@ -37,9 +37,9 @@ app.use('/users', usersRouter);
 app.use('/parkingBays', parkingBaysRouter);
 app.use('/update', databaseRouter);
 
-/*app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+ '../frontend/build/index.html'));
-});*/
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 
 // catch 404 and forward to error handler
