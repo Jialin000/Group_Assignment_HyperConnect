3bcb1e113a3fa23898e47a8bdaa78593416005ea
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-17 13:39:14 +1000

    test

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use('/', express.static(path.join(__dirname, '../frontend/build')));
 app.use(bodyParser.json());
 app.use(cors());
 
