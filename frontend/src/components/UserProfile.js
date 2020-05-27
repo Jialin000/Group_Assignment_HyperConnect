264e51a4dd0adc53b59db6113cda963f9f3653e4
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-27 22:00:45 +1000

    Update UserProfile.js

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -5,55 +5,73 @@ import Button from "../components/Button";
 
 
 export default function UserProfile(props) {
+    const { userName, email } = props;
+    
+    const [username_value, setUserName] = useState(userName);
+    const [email_value, setEmail] = useState(email);
     const [showUpdate, setShowUpdate] = useState(false);
+
+    function cancelSubmit(){
+        setUserName(userName);
+        setEmail(email);
+        setShowUpdate(!showUpdate);
+    }
+
+    
+    function onSubmit() {
+        updateUserProfile({
+            userName: username_value,
+            email: email_value
+        }).then(res => {
+            if (res.status == 200){
+                alert("Change has been saved!");
+                setShowUpdate(!showUpdate);
+            }else if(res.status == 401){
+                alert("Please log in");
+                window.location.replace("/users/login");
+            }else{
+                alert("Error updating, please try again");
+            }
+        })                      
+    }
+
+
+    function ProfileForm() {        
+        return (
+            <div className="ProfileForm">
+                <p>Username: {username_value}</p><br/>
+                <p>Email: {email_value}</p><br/>
+                <Button className={"btn"} onClick={()=>setShowUpdate(!showUpdate)}>
+                    Edit my profile
+                </Button>
+            </div>
+        );
+    }
+
+
+    function UpdateProfileForm() {
+        return (
+        <div className="UpdateProfileForm">
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username"  value={username_value} onChange={event => {setUserName(event.target.value)}}/><br/><br/>
+                <label htmlFor="email">Email:</label>
+                <input type="text" name="email" value={email_value} onChange={event => {setEmail(event.target.value)}}/><br/><br/>
+                <Button className={"btn-warning"} onClick={onSubmit}>
+                    Save
+                </Button>
+                <Button className={"btn"} onClick={cancelSubmit}>
+                    Cancel
+                </Button>
+            </form>
+        </div>
+        );
+    }
 
     return (
       <div className="userProfile">          
-        {showUpdate ? <UpdateProfileForm {...props} /> : <ProfileForm {...props} />}          
-        <Button className={"btn"} onClick={() => setShowUpdate(!showUpdate)}>
-            {showUpdate ? "Cancel" : "Edit profile"}
-        </Button>  
+        {showUpdate ? <UpdateProfileForm/> : <ProfileForm/>}                  
       </div>
     );
 }
-
-export function ProfileForm(props) {
-    
-    const { userName, email } = props;
-  
-    return (
-        <div className="ProfileForm">
-            <p>Username: {userName}</p><br/>
-            <p>Email: {email}</p><br/>
-        </div>
-    );
-}
-
-export function UpdateProfileForm(props) {
-    const { userName, email } = props;
-  
-    const [username_input, setUserName] = useState(userName);
-    const [email_input, setEmail] = useState(email);
-  
-    function onSubmit() {
-        updateUserProfile({
-            userName: username_input,
-            email: email_input
-        })       
-        .then(window.location.reload());
-    }
-  
-    return (
-      <div className="UpdateProfileForm">
-        <form>
-            <label htmlFor="username">Username:</label>
-            <input type="text" name="username"  value={username_input} onChange={event => {setUserName(event.target.value)}}/><br/><br/>
-            <label htmlFor="email">Email:</label>
-            <input type="text" name="email" value={email_input} onChange={event => {setEmail(event.target.value)}}/><br/><br/>
-            <Button className={"btn-warning"} onClick={onSubmit}>
-                Save
-            </Button>
-        </form>
-      </div>
-    );
-}
+ 
