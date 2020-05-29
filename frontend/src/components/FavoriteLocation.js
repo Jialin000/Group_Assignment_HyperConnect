79e751354208da812c591c33b5bce4391005e4b1
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-29 13:44:25 +1000

    Update FavoriteLocation.js

--- frontend/src/components/FavoriteLocation.js
+++ frontend/src/components/FavoriteLocation.js
@@ -10,30 +10,32 @@ export default function FavoriteLoactions(props) {
   	const [showUpdate, setShowUpdate] = useState(false);
 
 	// function to remove the svaed location
-	function deleteLocation(location){
+	function deleteFavoriteLocation(_id){
 		// make a copy of old array
 		// remove the location from the new array
-		var new_array = loaction_list;
-    	var index = new_array.indexOf(location);
+		//var new_array = loaction_list;
+    	//var index = new_array.indexOf();
 
-    	if (index > -1) {
+		deleteLocation(_id);
+
+    	/*if (index > -1) {
 			new_array.splice(index, 1);
 			setLocations(new_array);
 		} else{
 			alert("Error deleting");
-		}
+		}*/
 	}
 
 	// funtion to seacrh the parking bays around the saved locations
 	// redirect to the searching page
 	function searchLocation(){
-
+		alert("click");
 	}
 
 
-	function DeleteButton(){
+	function DeleteButton(_id){
 		return(
-			<Button className={"btn"} onClick={() => deleteLocation}>
+			<Button className={"btn"} onClick={(_id) => deleteFavoriteLocation}>
 				Delete
 			</Button>
 		);
@@ -42,30 +44,31 @@ export default function FavoriteLoactions(props) {
 
 	function SearchButton(){
 		return(
-			<Button className={"btn"} onClick={() => searchLocation}>
+			<Button className={"btn"} onClick={()=>searchLocation(this)}>
 				Search
 			</Button>
 		);
 	}
 
-	
+	// show a sigle location
 	function Location(props) {
-		const {tag, lat, lng} = props;
+		const {_id, tag, lat, lng} = props;
 		return (
 			<div className="location">
+				<p>{_id}</p>
 				<p>{tag}</p>
 				<p>{lat}</p>
 				<p>{lng}</p>
-				{showUpdate ? <DeleteButton/>: <SearchButton/>}
+				{showUpdate ? <DeleteButton {..._id}/>: <SearchButton/>}
 			</div>
 		);
 	}
 
-
+	// a list a saved locations
 	function FavoriteLoactionsList() {	
 		return(  
 		  	<div className="loaction_list">    
-				{loaction_list.map(location => (<Location {...location} />))} 
+				{loaction_list.map(location => (<Location key={location._id} {...location} />))} 
 		  	</div> 
 		); 
 	}
