f0723aacfe6c09df66cbff8ee48d29c8a7aff4f4
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-05-30 11:14:03 +1000

    Update SignInPage.js

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -7,17 +7,16 @@ import "../styles.css"
 export default function SignInPage() {
 
         return (
-      <div className="login_bg">
-          <div className="login_box">
-
-             <SignInForm />
-          </div>
-          <div className="login_description">
-              <h1>After logging in,
-                  <br/>you can save the locations as your favourites for later using.
-              </h1>
-          </div>
-      </div>
+            <div className="login_bg">
+                <div className="login_box">
+                    <SignInForm />
+                </div>
+                <div className="login_description">
+                    <h1>After logging in,
+                        <br/>you can save the locations as your favourites for later using.
+                    </h1>
+                </div>
+            </div>
 
     );
 }
