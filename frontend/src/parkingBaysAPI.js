63d247bb416a4ad605a61af713f273f34db166a7
Jialin000 <61930223+Jialin000@users.noreply.github.com>
2020-06-06 17:25:56 +1000

    update the database everytime

--- frontend/src/parkingBaysAPI.js
+++ frontend/src/parkingBaysAPI.js
@@ -7,12 +7,11 @@ import { useState, useEffect } from "react";
 // end point to get all parking bays
 export function getBays() {
   const endpoint = `/parkingBays`;
-  // fetch(`/update`);
-  return fetch(endpoint)
-    .then((res) => {
-      console.log(res);
-        return res.json();
-      });
+  fetch(`/update`);
+  return fetch(endpoint).then((res) => {
+    console.log(res);
+    return res.json();
+  });
 }
 
 // export function findBays(location) {
